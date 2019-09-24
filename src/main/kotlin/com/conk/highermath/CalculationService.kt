af83be98ee67f88b36433d7fecb2a2c33b9f6bc2
Heather Conklin <heath3conk@users.noreply.github.com>
2019-09-24 18:14:54 -0500

    Update CalculationService.kt

--- src/main/kotlin/com/conk/highermath/CalculationService.kt
+++ src/main/kotlin/com/conk/highermath/CalculationService.kt
@@ -3,6 +3,8 @@ package com.conk.highermath
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import kotlin.math.log
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 @Service
 class CalculationService {
@@ -12,9 +14,42 @@ class CalculationService {
     fun logYear(number: Int, base: Int = 10): Int {
         val baseYear = 1995
         val actualResult = log(number.toDouble(), base.toDouble()).toInt()
-        if (0 <= actualResult && actualResult <= 24) {
+        if (actualResult in 0..24) {
             return baseYear + actualResult
         }
-        return baseYear + actualResult.mod(24)
+        return baseYear + actualResult.rem(24)
+    }
+
+    fun modMonth(number: Int): Int {
+        val modOfNumber = 12.rem(number)
+        if (modOfNumber == 0) {
+            return 1
+        }
+        return modOfNumber
+    }
+
+    fun rootDay(number: Int): Int {
+        val squareRoot = sqrt(number.toDouble()).toInt()
+        if (squareRoot == 0) {
+            return 1
+        }
+        if (squareRoot > 31) {
+            return 31.rem(squareRoot)
+        }
+        return squareRoot
+    }
+
+    fun birthday(someDate: String): LocalDate {
+        val madeDate = LocalDate.parse(someDate)
+        if (withinDateRange(madeDate)) {
+            return madeDate
+        }
+
+        val randomYear = Random.nextInt(1995, 2020)
+        return madeDate
+    }
+
+    private fun withinDateRange(dateToCheck: LocalDate): Boolean {
+        return dateToCheck in earliestDate..LocalDate.now()
     }
 }
